09ed88b44e243f6713c9bd6decccb0e084a476cc
Nihed Majdoub <nmajdoub.ext@wedigital.garden>
2021-01-10 17:23:34 +0100

    add modelName

--- Sources/FHShared/FHShared.swift
+++ Sources/FHShared/FHShared.swift
@@ -6,13 +6,19 @@ public struct UpdateDevice: Codable {
   public let pushToken: String?
   public let system: Device.System
   public let osVersion: String
+  public let modelName: String
   public let channels: [String]?
 
-  public init(id: UUID? = nil, pushToken: String? = nil, system: Device.System, osVersion: String, channels: [String]? = nil) {
+    public init(id: UUID? = nil,
+                pushToken: String? = nil,
+                system: Device.System, osVersion: String,
+                modelName: String,
+                channels: [String]? = nil) {
     self.id = id
     self.pushToken = pushToken
     self.system = system
     self.osVersion = osVersion
+    self.modelName = modelName
     self.channels = channels
   }
 }
@@ -26,14 +32,21 @@ public struct Device: Codable {
   public let id: UUID
   public let system: System
   public var osVersion: String
+  public let modelName: String
   public var pushToken: String?
   public var channels: [String]
 
-  public init(id: UUID, system: System, osVersion: String, pushToken: String?, channels: [String]) {
+  public init(id: UUID,
+              system: System,
+              osVersion: String,
+              modelName: String,
+              pushToken: String?,
+              channels: [String]) {
     self.id = id
     self.system = system
     self.osVersion = osVersion
     self.pushToken = pushToken
+    self.modelName = modelName
     self.channels = channels
   }
 }
